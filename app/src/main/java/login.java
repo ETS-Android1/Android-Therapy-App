2b9e287b00365c3ab7dc2e93a8127f98d62c95f1
Muda-u <2094007@students.wits.ac.za>
2020-05-24 16:12:29 +0200

    start layouts

---
+++ app/src/main/java/login.java
@@ -0,0 +1,13 @@
+import android.app.Activity;
+import android.view.View;
+
+import com.example.counselor.R;
+
+public class login extends Activity {
+
+    @Override
+    public void setContentView(View view) {
+        super.setContentView(view);
+    }
+
+}
