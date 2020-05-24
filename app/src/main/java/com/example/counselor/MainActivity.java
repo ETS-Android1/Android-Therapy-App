acb87a8e9240963248cee65ed40fe17db64b0f82
Muda-u <2094007@students.wits.ac.za>
2020-05-24 16:08:44 +0200

    start layouts

--- app/src/main/java/com/example/counselor/MainActivity.java
+++ app/src/main/java/com/example/counselor/MainActivity.java
@@ -2,14 +2,27 @@ package com.example.counselor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView regy;
+    private Button bt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.register_therapist);
+
+        setContentView(R.layout.login);
+        regy = findViewById(R.id.register_txt);
+
+
+
+
     }
+
 
 }
