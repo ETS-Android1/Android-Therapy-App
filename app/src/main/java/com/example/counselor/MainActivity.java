816edd89a3cfd95dda804194a3ae1f2ead17f04e
Muda-u <2094007@students.wits.ac.za>
2020-05-26 00:53:00 +0200

    start layouts

--- app/src/main/java/com/example/counselor/MainActivity.java
+++ app/src/main/java/com/example/counselor/MainActivity.java
@@ -10,18 +10,38 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-//    private TextView regy;
-//    private Button bt;
+    private TextView regy;
+  private Button bt;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.login);
-//        regy = findViewById(R.id.register_txt);
+
+       regy = this.findViewById(R.id.register_txt);
+       bt = findViewById(R.id.login_btn);
+
+
+
+      regy.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               setlayout();
+
+
+           }
+       });
 
 
 
 
+
+    }
+    public void setlayout(){
+        Intent intent = new Intent(this , type_of_regi.class);
+        startActivity(intent);
     }
 
 
--- app/src/main/java/com/example/counselor/homeActivity.java
+++ app/src/main/java/com/example/counselor/homeActivity.java
@@ -1,6 +1,8 @@
 package com.example.counselor;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
