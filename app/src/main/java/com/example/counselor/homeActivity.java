2c627a1992962017e32e264f6927ef270545ca24
Muda-u <2094007@students.wits.ac.za>
2020-05-31 00:12:35 +0200

    start layouts

--- app/src/main/java/com/example/counselor/homeActivity.java
+++ app/src/main/java/com/example/counselor/homeActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,15 +16,23 @@ public class homeActivity extends AppCompatActivity {
     SessionManager sessionManager;
     Button logout;
     private Button chat;
+    private TextView helping,us;
     private ImageView settings, profile;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.home);
+        setContentView(R.layout.home2);
         sessionManager = new SessionManager(this);
         sessionManager.checkLogin();
         settings = findViewById(R.id.goToSettings);
+        us = findViewById(R.id.username);
+        helping = findViewById(R.id.helpingyou);
+        SessionManager n = new SessionManager(this);
+        HashMap<String, String> user = n.getUserDetail();
+        String username = user.get(n.USERNAME);
+        us.setText(username);
+
         profile = findViewById(R.id.profile);
         chat = findViewById(R.id.goToChat);
         chat.setOnClickListener(new View.OnClickListener() {
